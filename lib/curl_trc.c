5d51e5e891638a08a4d220af986daacc57c6ee67
Fujii Hironori <Hironori.Fujii@sony.com>
2025-02-05 16:33:07 +0900

    Use OutputDebugStringA in Curl_debug

--- lib/curl_trc.c
+++ lib/curl_trc.c
@@ -60,6 +60,7 @@
 void Curl_debug(struct Curl_easy *data, curl_infotype type,
                 char *ptr, size_t size)
 {
+  OutputDebugStringA(ptr);
   if(data->set.verbose) {
     static const char s_infotype[CURLINFO_END][3] = {
       "* ", "< ", "> ", "{ ", "} ", "{ ", "} " };
